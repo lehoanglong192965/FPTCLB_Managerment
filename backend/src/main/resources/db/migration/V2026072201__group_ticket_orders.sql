25d010f0b602823b2c1091719123844e33f7c00d
Kevin <lehoanglong19062005@gmail.com>
2026-07-22 01:30:30 +0700

    update db

--- backend/src/main/resources/db/migration/V2026072201__group_ticket_orders.sql
+++ backend/src/main/resources/db/migration/V2026072201__group_ticket_orders.sql
@@ -21,18 +21,4 @@ BEGIN
          WHERE isDeleted = 0
            AND userID IS NOT NULL
            AND purchaserUserID IS NULL;';
-
-    IF EXISTS (
-        SELECT 1 FROM sys.check_constraints
-        WHERE name = 'CK_EventRegistration_UserChannelConsistency'
-          AND parent_object_id = OBJECT_ID('dbo.EventRegistration')
-    )
-        ALTER TABLE dbo.EventRegistration DROP CONSTRAINT CK_EventRegistration_UserChannelConsistency;
-
-    ALTER TABLE dbo.EventRegistration ADD CONSTRAINT CK_EventRegistration_UserChannelConsistency
-        CHECK (
-            (registrationChannel = 'FPTU' AND userID IS NOT NULL)
-            OR (registrationChannel = 'ONLINE' AND userID IS NULL)
-            OR (registrationChannel = 'WALK_IN')
-        );
 END;
